74a78558f87dfaa5cf184571dbbcae32f117ca06
shyshkovskaliliia-art <shyshkovska.liliia@chnu.edu.ua>
2026-03-29 13:38:34 +0300

    add task1_part2(for task4)

---
+++ Lab5/task4/task1_part2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace lab5.task4
+{
+    internal sealed partial class Test
+    {
+        public int GetQuestionsCount()
+        {
+            return questionsCount;
+        }
+
+        public void SetQuestionsCount(int count)
+        {
+            if (count > 0)
+            {
+                questionsCount = count;
+                Console.WriteLine("Кількість питань оновлено: " + count);
+            }
+            else
+            {
+                Console.WriteLine("Помилка: кількість питань має бути > 0");
+            }
+        }
+    }
+
+    internal partial class Exam
+    {
+        public string GetSubject()
+        {
+            return subject;
+        }
+
+        public int GetDuration()
+        {
+            return duration;
+        }
+    }
+
+    internal sealed partial class FinalExam
+    {
+        public bool IsState()
+        {
+            return isStateExam;
+        }
+    }
+
+    internal sealed partial class Trial
+    {
+        public string GetTrialType()
+        {
+            return trialType;
+        }
+    }
+}
